126ea3ece8bdb196f60188ac947d6e19aeef62c3
AyaAhmed2021 <ayaahmed22793@gmail.com>
2021-03-22 15:04:34 +0200

    Banking Task

---
+++ New/js/script.js
@@ -0,0 +1,73 @@
+var addBtn = document.getElementById("button")
+var table = document.getElementById("tabData");
+var tbody = document.getElementById('tbody')
+// var row = table.insertRow() 
+ clients= []
+ client = {}
+
+function addClient(username , userid , userbalance) {
+    console.log(clients)
+    client ={
+        name : username,
+        id: userid,
+        balance : userbalance
+
+    }
+    clients.push(client)
+   
+    console.table(clients)
+}
+function removeUser(i){
+    clients.splice(i , 1)
+    showClient()
+}
+
+function editBalance(B){
+    B = prompt("enter the new balance")
+    showClient()
+}
+
+function showClient(){
+    tbody.textContent = ""
+
+    clients.forEach((item,i)=>{
+        var row = document.createElement('tr')
+        var td1 = document.createElement('td')
+        var td2 = document.createElement('td')
+        var td3 = document.createElement('td')
+        var td4 = document.createElement('td')
+        var td5 = document.createElement('td')
+        var removeBtn = document.createElement('button')
+        var editBtn = document.createElement('button')
+        removeBtn.innerText="remove user"
+        editBtn.innerText="edit user"
+        td1.innerText = i
+        td2.innerText = item.name
+        td3.innerText = item.id
+        td4.innerText = item.balance
+        td5.append(removeBtn)
+        td5.append(editBtn)
+        row.append(td1)
+        row.append(td2)
+        row.append(td3)
+        row.append(td4)
+        row.append(td5)
+        tbody.append(row)
+        removeBtn.addEventListener('click',function(){
+            removeUser(i)
+        })
+        editBtn.addEventListener('click',function(){
+             editBalance(item.balance)
+        })
+
+    })
+}
+addBtn.addEventListener('click', function() {
+    var user = prompt("enter User Name")
+    var id = prompt("enter id")
+    var balance = prompt("enter balance")
+
+    addClient(user, id, balance)
+    showClient()
+
+})
